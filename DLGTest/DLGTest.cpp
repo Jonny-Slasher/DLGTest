e1f046905e71dae1f43ce1f82f98f3d99aaeda1a
Jonny-Slasher <60716630+Jonny-Slasher@users.noreply.github.com>
2020-08-01 18:45:33 +0300

    Добавьте файлы проекта.

---
+++ DLGTest/DLGTest.cpp
@@ -0,0 +1,77 @@
+﻿#include"resource.h"
+#include <Windows.h>
+#include <iostream>
+
+HWND hEdit1;
+HWND hEdit2;
+WCHAR boofer[100];
+BOOL CALLBACK DlgProc1(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK DlgProc2(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE hInst;
+
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+    DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DlgProc1), 0);
+    return 0;
+}
+
+BOOL CALLBACK DlgProc1(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  
+    switch (uMsg) 
+    {
+    case WM_INITDIALOG: 
+        hInst = GetModuleHandle(NULL);
+        hEdit1 = GetDlgItem(hwnd, IDC_EDITONE);
+      // 
+        break;
+    case WM_COMMAND:
+        switch (LOWORD(wParam)) 
+    {
+    case ID_MODAL: {
+     
+        DialogBoxParam(hInst, MAKEINTRESOURCE(IDD_DIALOG2), hwnd, DlgProc2, 0);
+    }
+                 break;
+    }
+    break;
+    case WM_CLOSE: {
+        EndDialog(hwnd, 0);
+        return FALSE;
+    }
+    }
+    return FALSE;
+}
+BOOL CALLBACK DlgProc2(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    WCHAR first[100];
+    switch (uMsg) 
+    {
+    case WM_INITDIALOG: 
+        hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
+        SetFocus(hEdit2);
+      
+
+        break;
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case IDOK: {
+              SendMessage(hEdit2, WM_GETTEXT, (WPARAM)255, (LPARAM)first);
+              wsprintf(boofer, L"%s", first);
+              SendMessage(hEdit1, WM_SETTEXT, 0, (LPARAM)boofer);
+              EndDialog(hwnd, 0);
+           
+        }
+                     break;
+        }
+        break;
+    
+    break;
+    case WM_CLOSE: {
+        EndDialog(hwnd, 0);
+        return FALSE;
+    }
+    }
+    return FALSE;
+}
---
+++ DLGTest/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется DLGTest.rc
+//
+#define IDD_DIALOG1                     101
+#define IDD_DIALOG2                     103
+#define ID_MODAL                        1001
+#define ID_EXIT                         1002
+#define IDC_EDIT1                       1003
+#define IDC_EDITONE                     1003
+#define IDC_EDIT2                       1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
